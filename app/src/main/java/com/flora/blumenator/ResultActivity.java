a14ca8ea802b1bf842ee9b9c8c3bb767070257d3
Alina Dima <alinafdima@gmail.com>
2015-10-18 01:09:09 +0200

    Add links to result page

--- app/src/main/java/com/flora/blumenator/ResultActivity.java
+++ app/src/main/java/com/flora/blumenator/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.flora.blumenator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String flower = intent.getStringExtra("data");
+
+        final TextView tv = (TextView) findViewById(R.id.txt_result_name);
+        tv.setText(flower);
     }
 }
--- app/src/main/java/com/flora/blumenator/WaitingActivity.java
+++ app/src/main/java/com/flora/blumenator/WaitingActivity.java
@@ -1,5 +1,8 @@
 package com.flora.blumenator;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,23 @@ public class WaitingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
+
+        Intent intent = getIntent();
+        String imageStr = intent.getStringExtra("bitmapImage");
+        sendServerRequest(imageStr);
+    }
+
+    void sendServerRequest(String image_str) {
+        Bitmap image = BitmapFactory.decodeFile(image_str);
+
+        // Send server request here
+
+    }
+
+
+    void proceedWithResult(String flower_name) {
+        Intent intent = new Intent(WaitingActivity.this, WaitingActivity.class);
+        intent.putExtra("data",flower_name);
+        startActivity(intent);
     }
 }
